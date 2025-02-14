c20cf110a8e701e865bbae75059ff51aa8c7d9fd
shagun-nirvan <nirvanshagun173@gmail.com>
2025-02-14 14:18:32 +0530

    new commit

---
+++ Day2/MyFile.js
@@ -0,0 +1,5 @@
+const {sum,diff} =require("./MyMath");
+
+console.log(sum(10,20));
+
+console.log(diff(40,20));
---
+++ Day2/p7.js
@@ -0,0 +1,8 @@
+const fs=require("fs/promises");
+
+const read=async ()=>{
+    const data=await fs.readFile("./data.txt","utf-8");
+    console.log(data);
+}
+read();
+console.log("i am after reading")
